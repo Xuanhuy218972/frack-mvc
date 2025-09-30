9dbea246ffce47ff6277035bf5079a451c855fe2
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-09-30 14:39:36 +0700

    add order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,1 +1,15 @@
+class Order < ActiveRecord::Base
+  validates :user_id, presence: true
+  validates :order_date, presence: true
+  belongs_to :user
+
+  before_save :run_my_custom_validate
+
+  def run_my_custom_validate
+    if required_date && order_date && required_date < order_date
+      errors.add(:required_date, 'must be on or after order date')
+    end
+  end
+end
+
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-    
+  has_many :orders
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 end
